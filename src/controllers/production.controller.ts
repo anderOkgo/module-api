273c75e6ad37c7d82bb18addcc02a1aaebccc442
anderson rangel <andergroundmsg@gmail.com>
2023-03-11 02:01:53 +1000

    created method of searching

--- src/controllers/production.controller.ts
+++ src/controllers/production.controller.ts
@@ -2,13 +2,45 @@ import { Request, Response } from 'express';
 import connection from '../db/connection';
 
 export const getProductions = (req: Request, res: Response) => {
-  connection.query('SELECT name FROM productions', (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json({
-        data,
-      });
+  let conditions: string = '';
+  let sql: string;
+
+  const {
+    pro_name,
+    chapter_numer,
+    description,
+    YEAR,
+    demo_names,
+    genre_names,
+  } = req.body;
+
+  Object.keys(req.body).forEach((key) => {
+    if (req.body[key] === pro_name)
+      conditions = ` AND pro_name LIKE "%${pro_name}%"`;
+    if (req.body[key] === chapter_numer) {
+      let sp = chapter_numer.split(',');
+      if (sp.length == 1) {
+        conditions += ` AND chapter_numer = ${chapter_numer}`;
+      } else if (sp.length == 2) {
+        conditions += ` AND chapter_numer BETWEEN ${sp[0]} and ${sp[1]}`;
+      }
     }
+    if (req.body[key] === description)
+      conditions += ` AND description LIKE "%${description}%"`;
+    if (req.body[key] === YEAR) {
+      let sp = YEAR.split(',');
+      if (sp.length == 1) {
+        conditions += ` AND YEAR = ${YEAR}`;
+      } else if (sp.length == 2) {
+        conditions += ` AND YEAR BETWEEN ${sp[0]} and ${sp[1]}`;
+      }
+    }
+    if (req.body[key] === demo_names)
+      conditions += ` AND demo_names LIKE "%${demo_names}%"`;
+    if (req.body[key] === genre_names)
+      conditions += ` AND genre_names LIKE "%${genre_names}%"`;
   });
+  sql = 'SELECT * FROM v_all_info_produtions WHERE 1';
+  console.log(sql + conditions);
+  connection.query(sql + conditions, (err, data) => !err && res.json({ data }));
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 import Server from './models/server';
 
 // setting env vars
-dotenv.config();
+//dotenv.config();
 
 const server = new Server();

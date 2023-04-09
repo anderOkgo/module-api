9fdcc1f385bbd40e8d23b7fbfa301609d4b9ca20
anderson rangel <andergroundmsg@gmail.com>
2023-04-09 21:24:14 +1000

    validation bases 64 convertion nodejs

--- src/helpers/cyfer.ts
+++ src/helpers/cyfer.ts
@@ -15,11 +15,11 @@ export const cyfer = () => {
       li++;
       if (li > key.length - 1) li = 0;
     }
-    return btoa(nc).replaceAll('O', '-');
+    return Buffer.from(nc).toString('base64').replaceAll('O', '-');
   };
 
   const dcy = (str: string, key: string) => {
-    str = atob(str.replaceAll('-', 'O')).split('').reverse().join('');
+    str = Buffer.from(str, 'base64').toString().replaceAll('-', 'O').split('').reverse().join('');
     for (let x = str.length; x >= 2; x -= 2) {
       p2 = str.charCodeAt(x - 1) & 15;
       p3 = str.charCodeAt(x - 2) & 15;

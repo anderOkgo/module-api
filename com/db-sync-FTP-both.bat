5383cfb0a403a9cbd531ab54db1ea10560899e64
anderson rangel <andergroundmsg@gmail.com>
2023-06-28 01:26:25 +1000

    correct variable open link node

--- com/db-sync-FTP-both.bat
+++ com/db-sync-FTP-both.bat
@@ -1,7 +1,7 @@
 @ECHO Off
 SETLOCAL
-start "" https://nadim.dongee.com:2083/cpsess7273472010/frontend/jupiter/lveversion/nodejs-selector.html.tt#/applications/info.animecream.com
 IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
+start "" %Link_restart_node.txt%
 "keys/WinSCP.com" ^
   /command ^
     "open ftp://%FTPUsername.txt%:%FTPPss.txt%@%FTPserver.txt%" ^

dd42f37c87b71b5d3a8c6a28fafcc80b2c9270d3
anderson rangel <andergroundmsg@gmail.com>
2023-09-28 09:47:27 +1000

    change post

--- src/app/finan/application/finan.routes.ts
+++ src/app/finan/application/finan.routes.ts
@@ -3,7 +3,7 @@ import { router } from '../../../helpers/middle.helper';
 import { getTotalBank, putMoviment, defaultFInan } from './finan.controller';
 
 router.get('/finan', defaultFInan);
-router.get('/finan/totalbank', validateToken, getTotalBank);
+router.post('/finan/totalbank', validateToken, getTotalBank);
 router.put('/finan/insert', validateToken, putMoviment);
 
 export default router;

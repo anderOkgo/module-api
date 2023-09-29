1f00f1e77638c9701e38364d8f0bb6f9c4c82682
anderson rangel <andergroundmsg@gmail.com>
2023-09-29 18:01:12 +1000

    refactoring validate-token

--- src/helpers/lib/validate-token.ts
+++ src/helpers/lib/validate-token.ts
@@ -2,12 +2,8 @@ import { Request, Response, NextFunction } from '../middle.helper';
 import { token } from '../token.helper';
 
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.headers || !req.headers['authorization']) {
-    return res.status(401).json({
-      error: 'Unauthorized: Missing or invalid token format',
-    });
-  }
-  const headerToken = req.headers['authorization'];
+  // Check if the 'authorization' header is present
+  const headerToken = req.headers?.authorization;
 
   if (!headerToken || !headerToken.startsWith('Bearer ')) {
     return res.status(401).json({
@@ -16,28 +12,19 @@ const validateToken = (req: Request, res: Response, next: NextFunction) => {
   }
 
   const bearerToken = headerToken.slice(7); // Remove 'Bearer ' prefix
+
   try {
-    let valid = ''; //token.verify(bearerToken, process.env.SECRET_KEY || 'qwertgfdsa')
+    // Verify the token asynchronously
     token.verify(bearerToken, process.env.SECRET_KEY || 'qwertgfdsa', (err, decoded) => {
       if (err) {
         console.error('JWT verification failed:', err);
         return res.status(401).json({
           error: 'Unauthorized: Invalid token',
         });
-      } else {
-        valid = 'true';
-        // Handle valid token
       }
-    });
-    if (valid) {
       // Token is valid, proceed to the next middleware
       next();
-    } else {
-      // Token is invalid
-      return res.status(401).json({
-        error: 'Unauthorized: Invalid token',
-      });
-    }
+    });
   } catch (error) {
     // An error occurred while verifying the token
     console.error('Token verification error:', error);

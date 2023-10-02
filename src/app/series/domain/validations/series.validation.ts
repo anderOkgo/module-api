d277783f1297b5849e29dd54f4fe0a57a8d6a9d6
anderson rangel <andergroundmsg@gmail.com>
2023-10-02 11:59:13 +1000

    un commet

--- src/app/series/domain/validations/series.validation.ts
+++ src/app/series/domain/validations/series.validation.ts
@@ -8,15 +8,15 @@ function validate_id(id: string): boolean {
   return true;
 }
 
-/* function validate_production_name(production_name: string): boolean {
+function validate_production_name(production_name: string): boolean {
   if (!production_name || production_name.length < 1 || production_name.length > 100) {
     return false;
   }
 
   return true;
-} */
+}
 
-/* function validate_production_number_chapters(production_number_chapters: string): boolean {
+function validate_production_number_chapters(production_number_chapters: string): boolean {
   if (
     !production_number_chapters ||
     !Number.isInteger(production_number_chapters) ||
@@ -26,17 +26,17 @@ function validate_id(id: string): boolean {
   }
 
   return true;
-} */
+}
 
-/* function validate_production_description(production_description: string): boolean {
+function validate_production_description(production_description: string): boolean {
   if (!production_description || production_description.length < 1 || production_description.length > 1000) {
     return false;
   }
 
   return true;
-} */
+}
 
-/* function validate_production_year(production_year: string): boolean {
+function validate_production_year(production_year: string): boolean {
   if (
     !production_year ||
     !Number.isInteger(production_year) ||
@@ -47,24 +47,24 @@ function validate_id(id: string): boolean {
   }
 
   return true;
-} */
+}
 
-/* function validate_demographic_name(demographic_name: string): boolean {
+function validate_demographic_name(demographic_name: string): boolean {
   if (!demographic_name || demographic_name.length < 1 || demographic_name.length > 100) {
     return false;
   }
 
   return true;
-} */
+}
 
-/* function validate_genre_names(genre_names: string): boolean {
+function validate_genre_names(genre_names: string): boolean {
   if (!genre_names) {
     return true;
   } else {
     false;
-  } */
+  }
 
-/* const genre_names_array = genre_names.split(',');
+  const genre_names_array = genre_names.split(',');
 
   for (const genre_name of genre_names_array) {
     if (!genre_name || genre_name.length < 1 || genre_name.length > 100) {
@@ -73,25 +73,27 @@ function validate_id(id: string): boolean {
   }
 
   return true;
-} */
+}
 
-/* function validate_limit(limit: string): boolean {
+function validate_limit(limit: string): boolean {
   if (!limit || !Number.isInteger(limit) || parseInt(limit) < 1) {
     return false;
   }
 
   return true;
-} */
+}
 
 function validateProduction(production: Production): boolean {
-  return validate_id(production.id) /*&&
-    //validate_production_name(production.production_name) &&
+  return (
+    validate_id(production.id) &&
+    validate_production_name(production.production_name) &&
     validate_production_number_chapters(production.production_number_chapters) &&
     validate_production_description(production.production_description) &&
     validate_production_year(production.production_year) &&
     validate_demographic_name(production.demographic_name) &&
     validate_genre_names(production.genre_names) &&
-    //validate_limit(production.limit) */;
+    validate_limit(production.limit)
+  );
 }
 
 export default validateProduction;

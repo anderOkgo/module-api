96f1a5972793b3d1568fe752e95f54ee2e78ef3d
anderson rangel <andergroundmsg@gmail.com>
2024-05-09 15:48:50 -0500

    change route name

--- src/app/auth/application/user.controller.ts
+++ src/app/auth/application/user.controller.ts
@@ -7,25 +7,21 @@ export const defaultUsers = async (req: Request, res: Response) => {
 };
 
 export const addUsers = async (req: Request, res: Response) => {
-  // Validate user data
   const validation = validateUser(req.body);
   if (validation.error) {
     return res.status(400).json(validation);
   }
 
-  // If validation passes, proceed to addUser service
   const user = await addUser(req.body);
   !user.error ? res.status(200).json(user) : res.status(404).json(user);
 };
 
 export const loginUsers = async (req: Request, res: Response) => {
-  // Validate login data
   const validation = validateLogin(req.body);
   if (validation.error) {
     return res.status(400).json(validation);
   }
 
-  // If validation passes, proceed to loginUser service
   const login = await loginUser(req.body);
   login ? res.status(200).json(login) : res.status(404).json({ error: 'User not found' });
 };
--- src/app/auth/application/user.routes.ts
+++ src/app/auth/application/user.routes.ts
@@ -2,7 +2,7 @@ import { router } from '../../../helpers/middle.helper';
 import { addUsers, loginUsers, defaultUsers } from './user.controller';
 
 router.get('/', defaultUsers);
-router.post('/users', addUsers);
+router.post('/users/add', addUsers);
 router.post('/users/login', loginUsers);
 
 export default router;

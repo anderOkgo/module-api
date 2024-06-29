ecc75495b32fa01ced50b6c936d2f3ba65ed2558
anderson rangel <andergroundmsg@gmail.com>
2024-06-29 14:09:34 -0500

    init asus

--- 0.symbolics.bat
+++ 0.symbolics.bat
@@ -1,1 +1,1 @@
-G:/Mi unidad/Documents/Variant/Notes/Dev/Codes/Commands/FTPSync/module-api/0.symbolics.bat
+G:/My drive/Documents/Variant/Notes/Dev/Codes/Commands/FTPSync/module-api/0.symbolics.bat
--- keys/WinSCP.com
+++ keys/WinSCP.com
@@ -1,1 +1,1 @@
-G:/Mi unidad/Documents/Variant/Notes/Dev/Codes/Commands/FTPSync/module-api/keys/WinSCP.com
+G:/My drive/Documents/Variant/Notes/Dev/Codes/Commands/FTPSync/module-api/keys/WinSCP.com

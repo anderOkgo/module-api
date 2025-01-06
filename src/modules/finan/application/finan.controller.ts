0fcccbaa7c36167407c7d94ec8b1567108e78d68
anderson3A <anderson@tresastronautas.com>
2025-01-06 09:01:02 -0500

    sent array plain

--- src/modules/finan/application/finan.controller.ts
+++ src/modules/finan/application/finan.controller.ts
@@ -9,7 +9,7 @@ export const defaultFInan = async (res: Response) => res.json({ msg: `API Finan 
 
 export const getInitialLoad = async (req: Request, res: Response) => {
   const validation = validateGetInitialLoad(req.body);
-  if (validation.error) return res.status(400).json(validation);
+  if (validation.error) return res.status(400).json(validation.errors);
 
   const resp = await getInitialLoadService(req.body);
   return resp.errorSys ? res.status(500).json(resp.message) : res.status(200).json(resp);
@@ -17,7 +17,7 @@ export const getInitialLoad = async (req: Request, res: Response) => {
 
 export const putMovement = async (req: Request, res: Response) => {
   const validation = validatePutMovement(req.body);
-  if (validation.error) return res.status(400).json(validation);
+  if (validation.error) return res.status(400).json(validation.errors);
 
   const resp = await putMovementService(req.body);
   return resp.errorSys ? res.status(500).json(resp.message) : res.status(200).json(resp);
@@ -26,7 +26,7 @@ export const putMovement = async (req: Request, res: Response) => {
 export const updateMovement = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id, 10);
   const validation: ValidationResult = validateUpdateMovements(req.body, id);
-  if (validation.error) return res.status(400).json(validation);
+  if (validation.error) return res.status(400).json(validation.errors);
 
   const resp = await updateMovementService(id, req.body);
   return resp.errorSys ? res.status(500).json(resp.message) : res.status(200).json(resp);
@@ -36,7 +36,7 @@ export const deleteMovement = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id, 10);
   const username = req.body.username;
   const validation: ValidationResult = validateDeleteMovement(id);
-  if (validation.error) return res.status(400).json(validation);
+  if (validation.error) return res.status(400).json(validation.errors);
 
   const resp = await deleteMovementService(id, username);
   return resp.errorSys ? res.status(500).json(resp.message) : res.status(200).json(resp);
